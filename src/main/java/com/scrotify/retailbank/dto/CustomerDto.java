0ce8b3956e3b65dd9ba5b68c61f11cb7d3dd954b
scrotify <scrotifyteam@gmail.com>
2019-12-09 15:30:44 +0530

    customerDto change

--- src/main/java/com/scrotify/retailbank/dto/CustomerDto.java
+++ src/main/java/com/scrotify/retailbank/dto/CustomerDto.java
@@ -6,16 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CustomerDto {
-<<<<<<< HEAD
 	
 	private String customerName;
 	private Integer customerId;
 	private String customerEmail;
-	private String accountType;
-=======
-
-        private String customerName;
-	private Integer customerId;
-	private String customerEmail;
->>>>>>> 5fdc1d78b8cd2ce612a84b2be09328b80a103312
 }
